e123eac31f6f765808e1c7141b1c4b2cf4a8c753
RomannaBidnyk <romanna.bidnyk@gmail.com>
2022-09-29 19:01:46 +0300

    added icons, colors etc

--- app/src/main/java/com/example/weather_app/MainActivity.kt
+++ app/src/main/java/com/example/weather_app/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.weather_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import com.example.weather_app.presentation.ui.theme.WeatherAppTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            WeatherAppTheme {
+
+            }
+        }
     }
 }
